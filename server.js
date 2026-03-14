d0558c9c06f09a88eb815fea6c7fc657ac7a44e2
harshitrwt <harshit.rawat1804@gmail.com>
2026-03-14 23:49:19 +0530

    Initials

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-const express = require('express');
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';    
+
+dotenv.config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;  
+
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send("Hi from server");
-})
+    res.send('Hello, World!');
+});
 
-app.listen((port), () => {
-    console.log("Server runs on port " + port);
-})
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+}); 
